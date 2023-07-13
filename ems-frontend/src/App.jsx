8815428c27a59af558c20e4a28e4561624b3edc3
hemantDwivedi <hemantdwiwedi@gmail.com>
2023-07-13 17:58:03 +0530

    Configured: Routing in React App

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import FooterComponent from './component/FooterComponent'
 import HeaderComponent from './component/HeaderComponent'
@@ -6,9 +7,12 @@ import ListEmployeeComponent from './component/ListEmployeeComponent'
 function App() {
   return (
     <>
-    <HeaderComponent />
-    <ListEmployeeComponent />
-    <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes><Route path='/' element={<ListEmployeeComponent />}></Route></Routes>
+        <Routes><Route path='/employees' element={<ListEmployeeComponent />}></Route></Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
